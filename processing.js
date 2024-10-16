0cdd1fe57f5533790ae3f2040d8d1bbf962138dc
CrowMother <94501083+CrowMother@users.noreply.github.com>
2024-10-16 14:54:32 -0500

    updated Dicord channel variables and commenting for processing of data

--- processing.js
+++ processing.js
@@ -20,11 +20,19 @@ function format_webhook_data_new(data, suffix) {
 };
 
 
+/**
+ * Process an incoming webhook request.
+ * @param {Request} req The incoming request with the webhook data.
+ * @param {Response} res The response to send back to the client.
+ * @param {string} channelID The ID of the Discord channel to send the message to.
+ * @param {string} suffix The suffix to add to the message before sending it to Discord.
+ */
 async function process_webhook(req, res, channelID, suffix) {
   try {
     // Log incoming webhook data for debugging
     console.log('Webhook received:', req.body);
 
+    //console.log('Discord Token:', process.env.DISCORD_TOKEN);
     // Retrieve the Discord channel
     const channel = await discordClient.channels.fetch(channelID);
 
@@ -35,12 +43,11 @@ async function process_webhook(req, res, channelID, suffix) {
     // Send the formatted message to Discord
     await channel.send(messageContent);
 
-    // Respond to the client
-  // Respond to the client with a JSON object
-      res.status(200).json({ status: 'success', message: 'Webhook data received and sent to Discord' });
+    // Respond to the client with a JSON object
+    res.status(200).json({ status: 'success', message: 'Webhook data received and sent to Discord' });
   } catch (error) {
-  console.error('Error handling webhook:', error);
-  res.status(500).json({ status: 'error', message: 'Error processing webhook', error: error.toString() });
+    console.error('Error handling webhook:', error);
+    res.status(500).json({ status: 'error', message: 'Error processing webhook', error: error.toString() });
   }
 };
 
--- server.js
+++ server.js
@@ -19,11 +19,13 @@ app.use(express.json());
 
 // Route to handle incoming webhook data from Josh
 app.post('/trades/josh', async (req, res) => {
+  console.log(process.env.JOSH_DISCORD_CHANNEL_ID)
   process_webhook(req, res, process.env.JOSH_DISCORD_CHANNEL_ID, "@everyone")
 });
 
 //Route to handle incoming webhook data from Noob
 app.post('/trades/noob', async (req, res) => {
+  console.log(process.env.NOOD_DISCORD_CHANNEL_ID)
   process_webhook(req, res, process.env.NOOD_DISCORD_CHANNEL_ID, "")
 });
 
